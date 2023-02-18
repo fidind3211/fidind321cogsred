975297f06639a953a8778fb1e6ca17ab75cae3b0
fidind3211 <119537954+fidind3211@users.noreply.github.com>
2023-02-18 17:11:16 -0500

    PUSHHY

--- Tournament/tournament.py
+++ Tournament/tournament.py
@@ -1,53 +1,51 @@
-from redbot.core import commands
-from typing import List
 import discord
+from discord.ext import commands
 from random import shuffle
 
 class Tournament(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.participants = []
 
     @commands.command()
-    async def starttorny(self, ctx, num_participants: int, *participants: str):
-        if num_participants != len(participants):
-            await ctx.send("Number of participants and number of names provided do not match.")
+    async def starttorny(self, ctx, num_of_participants: int, *participants: str):
+        if num_of_participants <= 1:
+            await ctx.send("Tournament requires at least 2 participants.")
             return
-        if len(participants) < 2:
-            await ctx.send("A tournament needs at least 2 participants.")
+
+        if num_of_participants != len(participants):
+            await ctx.send("Number of participants does not match the given count.")
             return
+
         shuffle(participants)
-        self.participants = participants
-        await ctx.send("Tournament bracket has been generated and randomized!")
-        for i in range(0, len(self.participants), 2):
-            player1 = self.participants[i]
-            player2 = self.participants[i + 1]
-            match = f"{player1} VS {player2}"
-            await ctx.send(f"**Match {i//2 + 1}**: {match}")
 
-    @commands.command()
-    @commands.has_role("Tournament Admin")
-    async def win(self, ctx):
-        if len(self.participants) == 0:
-            await ctx.send("No tournament bracket is currently active.")
-            return
-        winner = self.participants.pop(0)
-        await ctx.send(f"{winner} has won the match!")
-        if len(self.participants) == 1:
-            await ctx.send(f"The winner of the tournament is: {self.participants[0]}")
-            self.participants = []
+        await ctx.send(f"Tournament bracket is completely randomized.\n{num_of_participants} participants are: {', '.join(participants)}")
 
-    @commands.command()
-    @commands.has_role("Tournament Admin")
-    async def lose(self, ctx):
-        if len(self.participants) == 0:
-            await ctx.send("No tournament bracket is currently active.")
+        for i in range(0, len(participants), 2):
+            user1 = participants[i]
+            user2 = participants[i+1]
+
+            message = await ctx.send(f"{user1} VS {user2}")
+
+            await message.add_reaction('🏆')
+            await message.add_reaction('❌')
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        message = reaction.message
+
+        if user == self.bot.user:
             return
-        self.participants.pop(0)
-        await ctx.send("Match has been forfeited.")
-        if len(self.participants) == 1:
-            await ctx.send(f"The winner of the tournament is: {self.participants[0]}")
-            self.participants = []
+
+        if reaction.emoji == '🏆' or reaction.emoji == '❌':
+            winner = None
+
+            if reaction.emoji == '🏆':
+                winner = message.content.split(' VS ')[0]
+            else:
+                winner = message.content.split(' VS ')[1]
+
+            await message.delete()
+            await message.channel.send(f"{winner} moves to the next round!")
 
 def setup(bot):
     bot.add_cog(Tournament(bot))
