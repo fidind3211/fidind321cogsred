247d9841bd67c0e3bdd15c5fa8bf3918385e5bb3
fidind3211 <119537954+fidind3211@users.noreply.github.com>
2023-03-02 14:06:26 -0500

    Now supports colors name using webcolors.

--- Post/post.py
+++ Post/post.py
@@ -1,5 +1,6 @@
 import discord
 from redbot.core import commands
+import webcolors
 
 class Post(commands.Cog):
     def __init__(self, bot):
@@ -7,11 +8,21 @@ class Post(commands.Cog):
     
     @commands.command()
     async def post(self, ctx, title, description, color, channel: discord.TextChannel):
-        embed = discord.Embed(title=title, description=description, color=int(color, 16))
+        try:
+            color_value = int(color, 16)
+        except ValueError:
+            try:
+                color_value = int(webcolors.name_to_hex(color).lstrip('#'), 16)
+            except ValueError:
+                await ctx.send("Invalid color value")
+                return
+
+        embed = discord.Embed(title=title, description=description, color=color_value)
         await channel.send(embed=embed)
-    
+
     @commands.command()
-    async def postmessage(self, ctx, message, channel: discord.TextChannel):
+    async def postmessage(self, ctx, *, message):
+        channel = ctx.channel
         await channel.send(message)
 
 def setup(bot):
