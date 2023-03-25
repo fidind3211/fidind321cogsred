89c2859576b006566b37a9a6372ce2596fa762c9
fidind3211 <119537954+fidind3211@users.noreply.github.com>
2023-03-25 12:55:51 -0400

    e

--- RandomApis/randomapis.py
+++ RandomApis/randomapis.py
@@ -50,5 +50,16 @@ class RandomApis(commands.Cog):
             data = io.BytesIO(await resp.read())
             await ctx.send(file=discord.File(data, 'simpcard.png'))
 
+     @commands.command()
+     async def tonikawa(self, ctx, *, user: commands.MemberConverter=None):
+        user = user or ctx.author
+        avatar_url = user.avatar_url_as(format='png', size=1024)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://some-random-api.ml/canvas/misc/tonikawa?avatar={avatar_url}") as resp:
+                if resp.status != 200:
+                    return await ctx.send('Error getting image...')
+                data = io.BytesIO(await resp.read())
+                await ctx.send(file=discord.File(data, 'tonikawa.png'))
+
 def setup(bot):
     bot.add_cog(RandomApis(bot))
