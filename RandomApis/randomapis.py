ddc421712562c6667bf14e7ae02614cb408cc6aa
fidind3211 <119537954+fidind3211@users.noreply.github.com>
2023-03-25 12:51:44 -0400

    e

--- RandomApis/randomapis.py
+++ RandomApis/randomapis.py
@@ -41,14 +41,14 @@ class RandomApis(commands.Cog):
 
     @commands.command()
     async def simp(self, ctx, *, user: commands.MemberConverter=None):
-        user = user or ctx.author
-        avatar_url = user.avatar_url
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://some-random-api.ml/canvas/misc/simpcard?avatar={avatar_url}") as resp:
-                if resp.status != 200:
-                    return await ctx.send('Error getting image...')
-                data = io.BytesIO(await resp.read())
-                await ctx.send(file=discord.File(data, 'simpcard.png'))
+     user = user or ctx.author
+     avatar_url = user.avatar_url_as(format='png', size=1024)
+     async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://some-random-api.ml/canvas/misc/simpcard?avatar={avatar_url}") as resp:
+            if resp.status != 200:
+                return await ctx.send('Error getting image...')
+            data = io.BytesIO(await resp.read())
+            await ctx.send(file=discord.File(data, 'simpcard.png'))
 
 def setup(bot):
     bot.add_cog(RandomApis(bot))
