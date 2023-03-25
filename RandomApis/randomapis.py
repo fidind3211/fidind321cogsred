f9f0e2aa6b89e036db7f4c87d9e52646ebd6418d
fidind3211 <119537954+fidind3211@users.noreply.github.com>
2023-03-25 12:42:39 -0400

    e

--- RandomApis/randomapis.py
+++ RandomApis/randomapis.py
@@ -27,11 +27,11 @@ class RandomApis(commands.Cog):
                     return await ctx.send('Error getting image...')
                 data = io.BytesIO(await resp.read())
                 await ctx.send(file=discord.File(data, 'nobitches.png'))
-
+    
     @commands.command()
     async def hornyjail(self, ctx, *, user: commands.MemberConverter=None):
         user = user or ctx.author
-        avatar_url = user.avatar_url
+        avatar_url = user.avatar_url_as(format='png', size=1024)
         async with aiohttp.ClientSession() as session:
             async with session.get(f"https://some-random-api.ml/canvas/overlay/jail?avatar={avatar_url}") as resp:
                 if resp.status != 200:
