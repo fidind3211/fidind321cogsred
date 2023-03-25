1be6fffbd1d55caf74fb507f4a98968a34213859
fidind3211 <119537954+fidind3211@users.noreply.github.com>
2023-03-25 12:11:14 -0400

    e

--- RandomApis/randomapis.py
+++ RandomApis/randomapis.py
@@ -19,9 +19,11 @@ class RandomApis(commands.Cog):
                 await ctx.send(file=discord.File(data, 'comrade.png'))
 
     @commands.command()
-    async def nobitches(self, ctx):
+    async def nobitches(self, ctx, *, user: commands.MemberConverter=None):
+        user = user or ctx.author
+        avatar_url = user.avatar_url_as(format='png', size=1024)
         async with aiohttp.ClientSession() as session:
-            async with session.get("https://some-random-api.ml/canvas/misc/nobitches") as resp:
+            async with session.get(f"https://some-random-api.ml/canvas/misc/nobitches?avatar={avatar_url}") as resp:
                 if resp.status != 200:
                     return await ctx.send('Error getting image...')
                 data = io.BytesIO(await resp.read())
